bb223bda095748c814b327fd353c5064ac57cf44
Amolnav <Alexiraptor06@gmail.com>
2025-05-26 10:37:43 +0200

    FIX 3.0 generadorAsientos

--- Triggers/generadorAsientos.sql
+++ Triggers/generadorAsientos.sql
@@ -5,21 +5,23 @@ AFTER INSERT ON vuelos
 FOR EACH ROW
 BEGIN
     DECLARE i INT DEFAULT 1;
-    DECLARE capacidad INT;
+    DECLARE cap_avion INT;
     DECLARE clase_asiento VARCHAR(50);
     DECLARE precio DECIMAL(10,2);
+    DECLARE msg_error VARCHAR(255);
 
-    /* Capacidad del avion */
-    SELECT capacidad INTO capacidad
+    /* Obtener capacidad del avión */
+    SELECT capacidad INTO cap_avion
     FROM aviones
-    WHERE idAvion = NEW.idAvion;
+    WHERE idAvion = NEW.idAvion
+    LIMIT 1;
 
-    WHILE i <= capacidad DO
+    WHILE i <= cap_avion DO
         /* Asignar clase y precio */
-        IF i <= capacidad * 0.2 THEN
+        IF i <= cap_avion * 0.2 THEN
             SET clase_asiento = 'Primera';
             SET precio = 300.00;
-        ELSEIF i <= capacidad * 0.5 THEN
+        ELSEIF i <= cap_avion * 0.5 THEN
             SET clase_asiento = 'Business';
             SET precio = 200.00;
         ELSE
@@ -27,9 +29,9 @@ BEGIN
             SET precio = 100.00;
         END IF;
 
-        /* Insertar Asiento */
+        /* Insertar asiento */
         INSERT INTO asiento (numeroAsiento, clase, precio, disponible, idVuelo)
-        VALUES (CONCAT('A', i), clase_asiento, precio, TRUE, NEW.idVuelo);
+        VALUES (CONCAT('A', i), clase_asiento, precio, 1, NEW.idVuelo);
 
         SET i = i + 1;
     END WHILE;
